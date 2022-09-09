59ec1993d8d059040690d49b52b98a14402e5052
Ieuan-Vinluan <96334572+Ieuan-Vinluan@users.noreply.github.com>
2022-09-09 08:23:07 +0800

    Add files via upload

--- Q1Ex1/Ex01TruthVinluan.java
+++ Q1Ex1/Ex01TruthVinluan.java
@@ -20,15 +20,18 @@ public class Ex01TruthVinluan {
 		schoolThree.wins = 8;
 		schoolThree.location = "Manila City";
 		
-		System.out.println("School 1\nName: " + schoolOne.name + "\nWins: " + schoolOne.wins + "\nCity: " + schoolOne.location + "\n");
-		System.out.println("School 2\nName: " + schoolTwo.name + "\nWins: " + schoolTwo.wins + "\nCity: " + schoolTwo.location + "\n");
-		System.out.println("School 3\nName: " + schoolThree.name + "\nWins: " + schoolThree.wins + "\nCity: " + schoolThree.location + "\n");
+		System.out.println("School 1\nName: " + schoolOne.name + "\nWins: " + schoolOne.wins + "\nLocation: " + schoolOne.location + "\n");
+		System.out.println("School 2\nName: " + schoolTwo.name + "\nWins: " + schoolTwo.wins + "\nLocation: " + schoolTwo.location + "\n");
+		System.out.println("School 3\nName: " + schoolThree.name + "\nWins: " + schoolThree.wins + "\nLocation: " + schoolThree.location + "\n");
 		
 		int totalWins = schoolOne.wins + schoolTwo.wins + schoolThree.wins; // (operation: addition)
 		boolean moreWins = schoolOne.wins > schoolTwo.wins; // (operation: comparison)
 		boolean sameCity = (schoolOne.location.equals(schoolTwo.location)) && (schoolTwo.location.equals(schoolThree.location)); // (operation: comparison)
 		
-		System.out.println("Total Wins: " + totalWins + " // (operation: addition)\nAteneo de Manila University has more wins than University of the Philippines: " + moreWins + " // (operation: comparison)\nAll three schools are located in the same city: " + sameCity + " // (operation: comparison)");
+//		System.out.println("Total Wins: " + totalWins + " // (operation: addition)\nAteneo de Manila University has more wins than University of the Philippines: " + moreWins + " // (operation: comparison)\nAll three schools are located in the same city: " + sameCity + " // (operation: comparison)");
+                System.out.println("Total Wins: " + totalWins + " // (operation: addition)");
+                System.out.println("Ateneo de Manila University has more wins than University of the Philippines: " + moreWins + " // (operation: comparison)");
+                System.out.println("All three schools are located in the same city: " + sameCity + " // (operation: comparison)");
 	}
 	
 }
