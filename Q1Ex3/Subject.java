16858aed2cac9875941c8085a37c6bd4b83cb4a7
Ieuan-Vinluan <96334572+Ieuan-Vinluan@users.noreply.github.com>
2022-09-23 07:50:34 +0800

    Add files via upload

--- Q1Ex3/Subject.java
+++ Q1Ex3/Subject.java
@@ -8,4 +8,11 @@ public class Subject {
         this.units = units;
         this.gradeLevel = gradeLevel;
     }
+    
+    // if no arguments are given
+    public Subject() {
+        name = "Mathematics 2";
+        units = 1.7;
+        gradeLevel = 8;
+    }
 }
