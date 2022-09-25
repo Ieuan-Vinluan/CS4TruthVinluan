1b73bcd2c3e1d0b60564783acf6ee8a78f248143
Ieuan-Vinluan <96334572+Ieuan-Vinluan@users.noreply.github.com>
2022-09-25 19:56:33 +0800

    Add files via upload

--- Q1Ex4/Singer.java
+++ Q1Ex4/Singer.java
@@ -3,14 +3,50 @@ public class Singer {
     private int noOfPerformances;
     private double earnings;
     private Song favoriteSong;
-    public static int totalPerformances = 0;
+    private static int totalPerformances = 0;
+    
+    public String getName() {
+		return name;
+	}
 
-    public Singer(String name, int noOfPerformances, double earnings, Song favoriteSong) {
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public int getNoOfPerformances() {
+		return noOfPerformances;
+	}
+
+	public void setNoOfPerformances(int noOfPerformances) {
+		this.noOfPerformances = noOfPerformances;
+	}
+
+	public double getEarnings() {
+		return earnings;
+	}
+
+	public void setEarnings(double earnings) {
+		this.earnings = earnings;
+	}
+
+	public Song getFavoriteSong() {
+		return favoriteSong;
+	}
+
+	public void setFavoriteSong(Song favoriteSong) {
+		this.favoriteSong = favoriteSong;
+	}
+	
+	public static int getTotalPerformances() {
+		return totalPerformances;
+	}
+
+	public Singer(String name, int noOfPerformances, double earnings, Song favoriteSong) {
         this.name = name;
         this.noOfPerformances = noOfPerformances;
         this.earnings = earnings;
         this.favoriteSong = favoriteSong;
-        totalPerformances += noOfPerformances;
+        Singer.totalPerformances += noOfPerformances;
     }
 
     // if no arguments are given
@@ -18,55 +54,28 @@ public class Singer {
         name = "Taylor Swift";
         noOfPerformances = 218;
         earnings = 23800000;
-        favoriteSong = new Song("Lover", "Taylor Swift", "flop", 3200000, 2019);
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public int getNoOfPerformances() {
-        return noOfPerformances;
-    }
-
-    public void setNoOfPerformances(int noOfPerformances) {
-        this.noOfPerformances = noOfPerformances;
-    }
-
-    public double getEarnings() {
-        return earnings;
-    }
-
-    public void setEarnings(double earnings) {
-        this.earnings = earnings;
-    }
-
-    public Song getFavoriteSong() {
-        return favoriteSong;
-    }
-
-    public void setFavoriteSong(Song favoriteSong) {
-        this.favoriteSong = favoriteSong;
+        favoriteSong = new Song("Lover", "Taylor Swift", 3200000, 2019);
+        Singer.totalPerformances += noOfPerformances;
     }
 
     public void performForAudience(int audience) {
-        noOfPerformances += 1; // increase number of performances by 1
-        earnings += 100 * audience; // increase earnings by 100 for each person
+        noOfPerformances += 1;
+        Singer.totalPerformances += 1;
+        earnings += 100 * audience;
     }
     
-    public void performForAudience(Singer anotherSinger, int audience) {
-        noOfPerformances += 1; // increase number of performances by 1
-        earnings += 100 * audience; // increase earnings by 100 for each person
-        
-        anotherSinger.noOfPerformances += 1; // increase number of performances by 1
-        anotherSinger.earnings += 100 * audience; // increase earnings by 100 for each person
+    // if performing with another singer
+    public void performForAudience(int audience, Singer singer) {
+    	noOfPerformances += 1;
+    	singer.noOfPerformances += 1;
+    	Singer.totalPerformances += 2;
+    	
+    	// profits are split equally
+    	earnings += (100 * audience) / 2;
+    	singer.earnings += (100 * audience) / 2;
     }
 
     public void changeFavSong(Song song) {
-        favoriteSong = song; // change favorite song
+        favoriteSong = song;
     }
 }
