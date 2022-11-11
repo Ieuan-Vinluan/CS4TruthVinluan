d1b15ec6609d10d138a926bb5bc034ff32a71389
Ieuan-Vinluan <96334572+Ieuan-Vinluan@users.noreply.github.com>
2022-11-11 08:19:27 +0800

    Add files via upload

---
+++ Q2Ex6/FireType.java
@@ -0,0 +1,17 @@
+package exercise06truthvinluanieuan;
+
+public class FireType extends Monster {
+
+	public FireType(String name, int maxHP, int base) {
+		super(name, "Fire", "Grass", "Water", maxHP, base);
+		this.setAtk((int) (1.3 * base));
+		this.setDef((int) (0.7 * base));
+	}
+	
+	@Override
+	public void special() {
+		this.setAtk(this.getAtk() + 2);
+		this.setHp(this.getHP() - 10 / 100 * this.getMaxHP());
+	}
+	
+}
