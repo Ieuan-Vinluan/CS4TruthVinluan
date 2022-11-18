d9da1d9a23d1dbb20d3802f4e0247297644bdc55
Ieuan-Vinluan <96334572+Ieuan-Vinluan@users.noreply.github.com>
2022-11-18 08:32:54 +0800

    Add files via upload

--- Q2Ex7/exercise07truthvinluanieuan/Main.java
+++ Q2Ex7/exercise07truthvinluanieuan/Main.java
@@ -3,11 +3,14 @@ package exercise07truthvinluanieuan;
 
 public class Main {
 	public static void main(String[] args) {
-		Trainer cheren = new Trainer("Cheren");
-		NPC testNPC = new NPC("Steven", "omg rocks wow");
-		Location home = new Location("Gonzaga", "overpriced burgers");
-		FireType cinderace = new FireType("ace of spades", 240, 130);
 		
+		// Create a Trainer, Monster, NPC, and Location.
+		Trainer cheren = new Trainer("Cheren");
+		NPC testNPC = new NPC("Steven", "Oh. My. ROCKS!! So RIVETING!!!");
+		Location home = new Location("Gonzaga", "overpriced burgers");
+		Monster cinderace = new FireType("Greninja", 240, 130);
+		
+		// Have the Trainer interact with each.
 		cheren.inspect(cinderace);
 		cheren.inspect(testNPC);
 		cheren.inspect(home);
